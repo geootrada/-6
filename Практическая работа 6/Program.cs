ea39778c01352f3dfae87943947057bab955dce1
geootrada <geootrada@yandex.ru>
2024-02-16 13:47:57 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = "Попов";
+            string surname = "Георгий";
+
+            Console.WriteLine($"Ваше имя {name}, ваша фамилия {surname}");
+
+            string temp;
+            temp = name;
+            name = surname;
+            surname = temp;
+
+            Console.WriteLine($"Прошу прощения, исправляю. Ваше имя {name}, ваша фамилия {surname}");
+        }
+    }
+}
